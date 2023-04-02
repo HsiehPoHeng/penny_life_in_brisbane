53626d6e92a3ae54b083a7506858fe4e5680f5e1
po-heng-home <>
2023-04-02 14:05:02 +0800

    更新首頁

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,14 +6,20 @@
       <a href="https://photos.google.com/share/AF1QipPIEgEPnIlNyvPXEcecKN8jmW6LVA1owBTEx5oEXLR1uQ4ekDfOeATi0-8oiQdtXw?key=eEtCYnRsWUlvemRBOXhKdWRPSVVNcHZ3c0wtYlJ3" target="_blank" rel="noopener">點我->>>相簿</a>.
     </p>
     <h3>各種風景</h3>    
+
+        <br>
+    <a href="https://photos.google.com/share/AF1QipPuMFqZiTW5KKQ7_-CxQSeXJ5bBnGWZ_4HUgdCCX150KsQ9FhMFcFkvOiE81co0fQ?key=VlRDSlA4TmwwOWIzaGc5MTVBSEx1QUdDVWtEMlNR" target="_blank" rel="noopener">點我->>>看風景</a>.
+  <br>
+ <br>
+  <br>
+   <br>
+    <br>
     <video width="320" height="240" controls>
   <source src="../assets/video/1680409743015.mp4" type="video/mp4">
   <source src="movie.ogg" type="video/ogg">
   Your browser does not support the video tag.
 </video>
-    <br>
-    <a href="https://photos.google.com/share/AF1QipPuMFqZiTW5KKQ7_-CxQSeXJ5bBnGWZ_4HUgdCCX150KsQ9FhMFcFkvOiE81co0fQ?key=VlRDSlA4TmwwOWIzaGc5MTVBSEx1QUdDVWtEMlNR" target="_blank" rel="noopener">點我->>>風景</a>.
-  </div>
+ </div>
 </template>
 
 <script>
