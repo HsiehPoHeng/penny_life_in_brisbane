3b654244d0df05dd93e08ce7e53ad6b17b460f80
po-heng-home <>
2023-04-02 13:50:50 +0800

    更新 首頁:放上影片連結

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,16 @@
     <h1>{{ msg }}</h1>
     <p>
       我的早餐
-      <a href="https://photos.google.com/share/AF1QipPIEgEPnIlNyvPXEcecKN8jmW6LVA1owBTEx5oEXLR1uQ4ekDfOeATi0-8oiQdtXw?key=eEtCYnRsWUlvemRBOXhKdWRPSVVNcHZ3c0wtYlJ3" target="_blank" rel="noopener">相簿</a>.
+      <a href="https://photos.google.com/share/AF1QipPIEgEPnIlNyvPXEcecKN8jmW6LVA1owBTEx5oEXLR1uQ4ekDfOeATi0-8oiQdtXw?key=eEtCYnRsWUlvemRBOXhKdWRPSVVNcHZ3c0wtYlJ3" target="_blank" rel="noopener">點我->>>相簿</a>.
     </p>
-    <h3>風景</h3>
+    <h3>各種風景</h3>    
+    <video width="320" height="240" controls>
+  <source src="../assets/video/1680409743015.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+  Your browser does not support the video tag.
+</video>
+    <br>
+    <a href="https://photos.google.com/album/AF1QipMHdeCrVsYTBYPMUcEj5XsIH0HMptVHWkdy1ZeE" target="_blank" rel="noopener">點我->>>風景</a>.
   </div>
 </template>
 
