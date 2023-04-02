cf49de9ef83356ecae51f824e4c930b376eab0d0
po-heng-home <>
2023-04-02 12:36:26 +0800

    1.更新 首頁 2.調整 baseUrl

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="https://lh3.googleusercontent.com/pw/AMWts8D7yG59hOfcGNDm4OrY77lApaNIlx117kTz1c7nuWfGU0GZ4k25SHkmckDK_-QqqvLbOol8holvCyxogNay6-BEhhlmPSEViWQWFD_SsaemguB2Fz6nxwzyLzgYqpN3zOvv_WeCBVaCD6ohj-Qo32tdLA=w1078-h809-s-no?authuser=0">
+  <HelloWorld msg="Penny 的澳洲生活"/>
 </template>
 
 <script>
